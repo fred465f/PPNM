2dfe9066fd3f3dfe5a7c380c5222784c789bb90f
fred465f <jessheimfred@gmail.com>
2024-02-06 13:55:04 +0100

    Worked on vec exercise, but currently has a problem with makefile

---
+++ exercises/vec/main.cs
@@ -0,0 +1,9 @@
+Using static vec;
+
+public static class main {
+    public static void Main() {
+        vec e1 = new vec(1, 0, 0);
+        vec e2 = new vec(0, 1, 0);
+        vec e3 = new vec(0, 0, 1);
+    }
+}
--- exercises/vec/vec.cs
+++ exercises/vec/vec.cs
@@ -2,7 +2,47 @@ using static System.Console;
 using static System.Math;
 
 public class vec {
-	static void Main() {
-		WriteLine("Hello!");
-	}
+	// Fields
+	public double x, y, z;
+
+	// Constructors
+    public vec() {x=y=z=0;}
+    public vec(double x, double y, double z) {this.x=x; this.y=y; this.z=z;}
+
+    // Overload usefull operators
+    public static vec operator+(vec v, vec u) {
+        return new vec(v.x+u.x, v.y+u.y, v.z+u.z);
+    }
+    public static vec operator-(vec v, vec u) {
+        return new vec(v.x-u.x, v.y-u.y, v.z-u.z);
+    }
+    public static vec operator*(vec v, double c) {
+        return new vec(c*v.x, c*v.y, c*v.z);
+    }
+    public static vec operator*(double c, vec v) {
+        return v*c;
+    }
+    public static vec operator-(vec u) {
+        return new vec(-u.x, -u.y, -u.z);
+    }
+
+    // Print method for debugging
+    public void print(string s) {
+        Write(s);
+        WriteLine($"{x} {y} {z}");
+    }
+    public void print() {
+        this.print("");
+    }
+
+    // Dot products, vector products and norms
+    public double dot(vec other) {
+        return this.x*other.x + this.y*other.y + this.z*other.z;
+    }
+    public static double dot(vec v, vec u) {
+        return v.x*u.x + v.y*u.y + v.z*u.z;
+    }
+    public double norm() {
+        return Sqrt(vec.dot(this, this));
+    }
 }
