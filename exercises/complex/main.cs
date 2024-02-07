5b9067921f30b7d1e93dddde53c8c9e64d692f9a
fred465f <jessheimfred@gmail.com>
2024-02-07 17:15:33 +0100

    Worked on complex exercise and had errors in second part, where I tried using partial classes to implement sinh and cosh.

---
+++ exercises/complex/main.cs
@@ -0,0 +1,59 @@
+using static System.Console;
+using static System.Math;
+
+public static class main {
+	public static void Main() {
+		// Part 1
+		WriteLine("----- Part 1 -----");
+		complex a1 = new complex(-1, 0);
+		complex a2 = cmath.sqrt(a1);
+		complex I = cmath.I;
+		complex b1 = cmath.sqrt(I);
+		complex c1 = cmath.exp(I);
+		complex d1 = cmath.exp(I*PI);
+		complex e1 = cmath.pow(I, I);
+		complex f1 = cmath.log(I);
+		complex g1 = cmath.sin(I*PI);
+		WriteLine($"Sqrt of -1 is = {a2}, which is {(approx(a2, I)).ToString()}");
+		WriteLine($"Sqrt of i is = {b1}, which is {(approx(b1, new complex(1/Sqrt(2), 1/Sqrt(2)))).ToString()}");
+		WriteLine($"Exp of i is = {c1}, which is {(approx(c1, new complex(Cos(1), Sin(1)))).ToString()}");
+		WriteLine($"Exp of i * pi is = {d1}, which is {(approx(d1, new complex(-1, 0))).ToString()}");
+		WriteLine($"i to the power of i is = {e1}, which is {(approx(e1, new complex(Exp(-PI/2), 0))).ToString()}");
+		WriteLine($"Log of i is = {f1}, which is {(approx(f1, new complex(0, PI/2))).ToString()}");
+		WriteLine($"Sin of i is = {g1}, which is {(approx(g1, new complex(0, Sinh(PI)))).ToString()}");
+
+		// Part 2
+	}
+	/*
+	public static partial class cmath {
+		public static complex sinh(complex z) {
+			double x=z.Re, y=z.Im;
+			return new complex(Sinh(x)*Cos(y), Cosh(x)*sin(y));
+		}
+		public static double sinh(double x) {return Sinh(x);}
+		public static complex cosh(complex z) {
+			double x=z.Re, y=z-Im;
+			return new complex(Cosh(x)*cos(y), Sinh(x)*sin(y));
+		}
+		public static double cosh(double x) {return Cosh(x);}
+	} */
+	
+	public static bool approx(complex x, complex y, double acc=1e-9, double err=1e-9) {
+		if (!( (Abs(x.Re-y.Re)<acc) || (Abs(x.Re-y.Re)<Max(Abs(x.Re),Abs(y.Re))*err) )) return false;
+		else if (!( (Abs(x.Im-y.Im)<acc) || (Abs(x.Im-y.Im)<Max(Abs(x.Im),Abs(y.Im))*err) )) return false;
+		else return true;
+	}
+}
+
+public static partial class cmath {
+	public static complex sinh(complex z) {
+		double x=z.Re, y=z.Im;
+		return new complex(Sinh(x)*Cos(y), Cosh(x)*sin(y));
+	}
+	public static double sinh(double x) {return Sinh(x);}
+	public static complex cosh(complex z) {
+		double x=z.Re, y=z-Im;
+		return new complex(Cosh(x)*cos(y), Sinh(x)*sin(y));
+	}
+	public static double cosh(double x) {return Cosh(x);}
+}
