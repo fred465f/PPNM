f98d39d7b18652b3289f6edd077f869f479f8b07
fred465f <jessheimfred@gmail.com>
2024-02-28 10:12:05 +0100

    Opened project for least_squares homework.

---
+++ homework/least_squares/main.cs
@@ -0,0 +1,12 @@
+using System;
+using static System.Math;
+using static System.Console;
+using LinearAlgebra;
+
+class Program
+{
+    public static void Main()
+    {
+        WriteLine("Hello!");
+    }
+}
