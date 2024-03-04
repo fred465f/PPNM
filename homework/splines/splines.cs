44459b2a5ffe4f4e0b39267f61f35446e5c87a43
fred465f <jessheimfred@gmail.com>
2024-03-04 11:02:52 +0100

    Worked on quadratic spline exercise and implemented most functionality. Seems to be some issue tho, when comparing with analytical results.

--- homework/splines/splines.cs
+++ homework/splines/splines.cs
@@ -102,14 +102,106 @@ public class LinearSpline
 public class QuadraticSpline
 {
     // Fields.
+    public Vector x, y, b, c;
 
     // Constructor.
+    public QuadraticSpline(Vector xValues, Vector yValues)
+    {
+        // Initialize memory.
+        x = xValues.Copy();
+        y = yValues.Copy();
+        b = new Vector(x.Length - 1);
+        c = new Vector(x.Length - 1);
+
+        // Initialize memory to vectors used in computation
+        Vector p = new Vector(x.Length - 1);
+        Vector h = new Vector(x.Length - 1);
+        for (int i = 0; i < x.Length - 1; i++)
+        {
+            h[i] = x[i + 1] - x[i];
+            p[i] = (y[i + 1] - y[i]) / h[i];
+        }
+
+        // Up recursion with c_0 = 0.
+        c[0] = 0;
+        for (int i = 0; i < x.Length - 2; i++)
+        {
+            c[i + 1] = (p[i + 1] - p[i] - c[i] * h[i]) / h[i + 1];
+        }
+
+        // Down recursion with c_(x.Length-1) := c_(x.Length-1)/2.
+        c[x.Length - 2] /= 2.0;
+        for (int i = x.Length - 3; i >= 0; i--)
+        {
+            c[i] = (p[i + 1] - p[i] - c[i + 1] * h[i + 1]) / h[i];
+        }
+
+        // Computing b.
+        for (int i = 0; i < x.Length - 1; i++)
+        {
+            b[i] = p[i] - c[i] * h[i];
+        }
+    }
 
     // Do interpolation using quadratic splines.
+    public double Interpolate(double z)
+    {
+        int i = BinarySearch(x, z);
+        return y[i] + b[i] * (z - x[i]) + c[i] * Pow(z - x[i], 2);
+    }
 
     // Perform integration from first x-value x[0] to chosen z > x[0].
+    public double Integrate(double z)
+    {
+        int i = BinarySearch(x, z);
+        double integral = 0;
+        for (int j = 0; j <= i; j++)
+        {
+            if (i == j)
+            {
+                integral += y[j] * z + 1.0/2.0 * b[j] * Pow(z - x[j], 2) + 1.0/3.0 * c[j] * Pow(z - x[j], 3);
+                integral -= y[j] * x[j];
+            }
+            else
+            {
+                integral += y[j] * x[j + 1] + 1.0/2.0 * b[j] * Pow(x[j + 1] - x[j], 2) + 1.0/3.0 * c[j] * Pow(x[j + 1] - x[j], 3);
+                integral -= y[j] * x[j];
+            }
+        }
+        return integral;
+    }
 
     // Perform differentiation from first x-value x[0] to chosen z > x[0].
+    public double Derivative(double z)
+    {
+        int i = BinarySearch(x, z);
+        return b[i] + 2.0 * c[i] * (z - x[i]);
+    }
 
     // Perform binary search.
+    public static int BinarySearch(Vector xValues, double z)
+    {
+        if (!(z >= xValues[0] && z <= xValues[xValues.Length - 1]))
+        {
+            throw new ArgumentException("Chosen z-value must lie in interval [xValues[0], xValues[Length - 1]].", $"z-value = {z} does not lie in interval [{xValues[0]}, {xValues[xValues.Length - 1]}].");
+        }
+        else
+        {
+            int i = 0;
+            int j = xValues.Length - 1;
+            while (j - i > 1)
+            {
+                int middle = (i + j) / 2;
+                if (z > xValues[middle])
+                {
+                    i = middle;
+                }
+                else
+                {
+                    j = middle;
+                }
+            }
+            return i;
+        }
+    }
 }
--- homework/splines/testQuadraticSpline.cs
+++ homework/splines/testQuadraticSpline.cs
@@ -7,27 +7,77 @@ using static System.Console;
 
 class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        // Variables.
-        string inFile = "";
-
-        // Parse command line inputs.
-        foreach (var arg in args)
+        // Create dataset 1 to be tested.
+        Vector xValues1 = new Vector(5);
+        Vector yValues1 = new Vector(5);
+        for (int i = 0; i < 5; i++)
         {
-            var words = arg.Split(":");
-            if (words[0] == "-inFile")
-            {
-                inFile = words[1];
-            }
+            xValues1[i] = i;
+            yValues1[i] = 1;
         }
+        Vector b1Analytical = new Vector("0\n0\n0\n0");
+        Vector c1Analytical = new Vector("0\n0\n0\n0");
 
-        // Load in data to be tested.
-        Matrix inputData = new Matrix(inFile);
-        Vector xValues = inputData[0]; // xValues in range [?, ?].
-        Vector yValues = inputData[1]; // f(x) = ?
+        // Create dataset 2 to be tested.
+        Vector xValues2 = new Vector(5);
+        Vector yValues2 = new Vector(5);
+        for (int i = 0; i < 5; i++)
+        {
+            xValues1[i] = i;
+            yValues1[i] = i;
+        }
+        Vector b2Analytical = new Vector("1\n1\n1\n1");
+        Vector c2Analytical = new Vector("1\n1\n1\n1");
 
-        // Create data to be uploaded.
-        WriteLine("#xValues,yValues,integral,differential");
+        // Create dataset 3 to be tested.
+        Vector xValues3 = new Vector(5);
+        Vector yValues3 = new Vector(5);
+        for (int i = 0; i < 5; i++)
+        {
+            xValues1[i] = i;
+            yValues1[i] = i * i;
+        }
+        Vector b3Analytical = new Vector("1\n1\n1\n1");
+        Vector c3Analytical = new Vector("1\n1\n1\n1");
+
+        // Compute parameters b and c of quadratic splines for each dataset numerically.
+        (Vector b1, Vector c1) = ComputeQuadraticSplineParameters(xValues1, yValues1);
+        (Vector b2, Vector c2) = ComputeQuadraticSplineParameters(xValues2, yValues2);
+        (Vector b3, Vector c3) = ComputeQuadraticSplineParameters(xValues3, yValues3);
+
+        // Compare numerically and analytically calculated results.
+        if (Vector.Approx(b1, b1Analytical) && Vector.Approx(c1, c1Analytical))
+        {
+            WriteLine("Numerically computed parameters of quadratic splines agreed with the analytically computed ones.");
+        }
+        else
+        {
+            WriteLine("Numerically computed parameters of quadratic splines did not agree with the analytically computed ones.");
+        }
+        if (Vector.Approx(b2, b2Analytical) && Vector.Approx(c2, c2Analytical))
+        {
+            WriteLine("Numerically computed parameters of quadratic splines agreed with the analytically computed ones.");
+        }
+        else
+        {
+            WriteLine("Numerically computed parameters of quadratic splines did not agree with the analytically computed ones.");
+        }
+        if (Vector.Approx(b3, b3Analytical) && Vector.Approx(c3, c3Analytical))
+        {
+            WriteLine("Numerically computed parameters of quadratic splines agreed with the analytically computed ones.");
+        }
+        else
+        {
+            WriteLine("Numerically computed parameters of quadratic splines did not agree with the analytically computed ones.");
+        }
+    }
+
+    // Function computes quadratic spline parameters b, c.
+    public static (Vector b, Vector c) ComputeQuadraticSplineParameters(Vector xValues, Vector yValues)
+    {
+        QuadraticSpline quadraticSpline = new QuadraticSpline(xValues, yValues);
+        return (quadraticSpline.b, quadraticSpline.c);
     }
 }
