64a995fc8a67b77fd434fab221d4de1f501de362
fred465f <jessheimfred@gmail.com>
2024-05-11 11:13:09 +0200

    Started working on FFT algorithm and implemented it, but have not tested it yet.

---
+++ Libraries/Calculus/FFT.cs
@@ -0,0 +1,119 @@
+/* FourierTransform class in Calculus namespace implements the Cooley-Tukey algorithm for
+the fast Fourier transform as well as the classical discrete Fourier transform as well as
+their respective inverses. */
+
+using System;
+using System.Numerics;
+using static System.Math;
+using static System.Console;
+
+namespace Calculus
+{
+    public static class FourierTransform
+    {
+        /* Computes FFT of input data and returns result in new complex array. */
+        public static Complex[] FFT(Complex[] x)
+        {
+            // Variables.
+            int N = x.Length;
+            Complex[] c = new Complex[N];
+
+            // Do FFT.
+            FFT(-1, N, x, 0, 1, c, 0);
+
+            // Return result.
+            return c;
+        }
+
+        /* Computes inverse FFT of input data and returns result in new complex array. */
+        public static Complex[] InverseFFT(Complex[] c)
+        {
+            // Variables.
+            int N = c.Length;
+            Complex[] x = new Complex[N];
+
+            // Do inverse FFT.
+            FFT(1, N, c, 0, 1, x, 0);
+            for (int i = 0; i < N; i++)
+            {
+                x[i] /= N;
+            }
+
+            // Return result.
+            return x;
+        }
+
+        /* Computes DFT of input data and returns result in new complex array. */
+        public static Complex[] DFT(Complex[] x)
+        {
+            // Variables.
+            int N = x.Length;
+            Complex[] c = new Complex[N];
+
+            // Do DFT.
+            DFT(-1, N, x, 0, 1, c, 0);
+
+            // Return result.
+            return c;
+        }
+
+        /* Computes inverse DFT of input data and returns result in new complex array. */
+        public static Complex[] InverseDFT(Complex[] c)
+        {
+            // Variables.
+            int N = c.Length;
+            Complex[] x = new Complex[N];
+
+            // Do inverse DFT.
+            DFT(1, N, c, 0, 1, x, 0);
+            for (int i = 0; i < N; i++)
+            {
+                x[i] /= N;
+            }
+
+            // Return result.
+            return x;
+        }
+        
+        /* Computes the classical discrete Fourier transform of input data and stores
+        result in input complex array. */
+        private static void DFT(int sign, int N, Complex[] x, int ix, int stride, Complex[] c, int ic)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                c[ic + k] = 0;
+                for (int n = 0; n < N; n++)
+                {
+                    c[ic + k] += x[ix + n*stride] * Complex.Exp(sign*2*PI*Complex.ImaginaryOne*n*k/N);
+                }
+            }
+        }
+
+        /* Computes fast Fourier transform using Cooley-Tukey algorithm on input data
+        and stores result in input complex array. It will be computed recursively, with
+        the base case N = 1 being trivial. */
+        private static void FFT(int sign, int N, Complex[] x, int ix, int stride, Complex[] c, int ic)
+        {
+            if (N == 1)
+            {
+                c[ic] = x[ix];
+            }
+            else if (N%2 == 0)
+            {
+                FFT(sign, N/2, x, ix, 2*stride, c, ic);
+                FFT(sign, N/2, x, ix + stride, 2*stride, c, ix + N/2);
+                for (int k = 0; k < N/2; k++)
+                {
+                    Complex p = c[ic + k];
+                    Complex q = Complex.Exp(sign*2*PI*Complex.ImaginaryOne*k/N) * c[ic + k + N/2];
+                    c[ic + k] = p + q;
+                    c[ic + k + N/2] = p - q;
+                }
+            }
+            else
+            {
+                DFT(sign, N, x, ix, stride, c, ic);
+            }
+        }
+    }
+}
---
+++ exam_preparation/15_FFT/testFFT.cs
@@ -0,0 +1,24 @@
+/* Program tests implementation of Cooley-Tukey FFT algorithm in FFT class in
+Calculus namespace. It does so by applying it to analytic know cases as well as
+checking that it is unitary. */
+
+using System;
+using Calculus;
+using System.Numerics;
+using static System.Math;
+using static System.Console;
+
+class Program
+{
+    public static void Main()
+    {
+        // Variables.
+        int N = 100;
+
+        /* Check FFT and DFT on known cases such as, f = A*cos(a*t) + B*cos(b*t) + C*cos(c*t), which in frequency 
+        space should have a distinct peak at a, b and c with distinct amplitudes. */
+        Func<double, double> f = t => 10*Cos(3*t) + 2*Cos(5*t) + 7*Cos(9*t);
+
+
+    }
+}
